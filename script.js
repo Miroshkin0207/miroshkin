a73fbb9fe77d72d8b3d011e79304b4606ed82ac1
Miroshkin0207 <vovamiroshkin2011@gmail.com>
2025-06-04 21:23:11 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function najatie() {
+    let nadpis = document.getElementById("nadpis")
+    let knopka = document.getElementById("knopka")
+    if (nadpis.innerHTML==="") {
+    knopka.innerHTML="Вернуть"
+    nadpis.innerHTML="Молодец!"
+    }else if (nadpis.innerHTML==="Молодец!") {
+    knopka.innerHTML="Нажми"
+    nadpis.innerHTML=""
+    }
+}
+function history() {
+window.location.href="https://miroshkin0207.github.io/history/"
+}
+function kalkulator() {
+window.location.href="https://miroshkin0207.github.io/kalkulator/"
+}
+function kliker() {
+window.location.href="https://miroshkin0207.github.io/kliker/"
+}
