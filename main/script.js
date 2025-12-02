8ef4b7d6fb66b9f6c5c9d422ddf6e422b74552e2
Miroshkin <vovamiroshkin2011@gmail.com>
2025-12-02 21:38:52 +0300

    Рефакторинг главной страницы

--- main/script.js
+++ main/script.js
@@ -1,12 +1,40 @@
 var quote = document.createElement("p")
 quote.id = "quote"
-document.querySelector("body").insertBefore(quote, document.querySelector("nav"))
+document.querySelector(".quoteContainer").appendChild(quote)
 var footer = document.querySelector("footer")
+const quotes = [
+    "Ненависть порождает ненависть.",
+    "Не ошибается только тот, кто ничего не делает.",
+    "Боль — это боль, как её ни назови. Разве она станет слабее от мысли, что она нереальна?",
+    "Жизнь человека определяется тем, что он считает истинным и правильным. Это и формирует нашу реальность. Вот только что такое истина? Всего лишь понятие, реальность может оказаться миражом. А быть может, люди живут в мире собственных иллюзий?",
+    "Те, кто способны простить себя и принять свою натуру, поистине сильны.",
+    "Знание не есть ум.",
+    "Кто хочет делать — ищет способ, кто не хочет — ищет причину.",
+    "Не бывает безвыходных ситуаций. Бывают ситуации, выход из которых тебя не устраивает.",
+    "Надо любить жизнь больше, чем смысл жизни.",
+    "Понимание — начало согласия."   
+]
 
+function getRandom(min, max) {
+    return min + Math.floor(Math.random() * max - min + 1)
+}
+
+let before = -1
+let n = -2
 function mainClick() {
-    quote.innerHTML = "Молодец!"   
-    if (window.matchMedia("(min-width: 1000px") && quote.offsetHeight > window.innerHeight * 0.4){
-        footer.style = "position: relative"
+    footer.classList.remove("relativeFooter")
+    n = getRandom(0, quotes.length - 1)
+    if (n == before) {
+        const half = Math.floor(quotes.length / 2)
+        n = (n >= half) ? getRandom(0, half - 1) : getRandom(half, quotes.length - 1)
+    }
+    before = n
+    quote.innerHTML = quotes[n]  
+    if (window.matchMedia("min-width: 1000px") && quote.offsetHeight > window.innerHeight) {
+        footer.classList.add("relativeFooter")
+    }
+    else if (window.matchMedia("max-width: 999px") && quote.offsetHeight > window.innerHeight * 0.5) {
+        footer.classList.add("relativeFooter")
     }
     document.querySelector("#mainButton").innerHTML = "Повторить"
 }
