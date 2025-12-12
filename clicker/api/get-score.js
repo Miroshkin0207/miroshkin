9723e3581d659dbc4522a8c0ac51d52faf5ac48c
Miroshkin <vovamiroshkin2011@gmail.com>
2025-12-12 22:43:48 +0300

    Ну же

--- clicker/api/get-score.js
+++ clicker/api/get-score.js
@@ -4,7 +4,14 @@ export default function getScore(req, res) {
         return res.send("0")
     }
     const scoreIndex = cookieString.indexOf("score=")
-    let score = scoreIndex != -1 ? cookieString.substring(scoreIndex + 6) : 0
+    let score1 = scoreIndex != -1 ? cookieString.substring(scoreIndex + 6) : 0
+    let score = ""
+    for (let i = 0; i < score1.length; i++) {
+        if (score1[i] == ";") {
+            break
+        }
+        score += score1[i]
+    }
 
     return res.send(score)
 }
