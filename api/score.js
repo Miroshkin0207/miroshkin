e454a9680868bd6e2cae6de6bb9cf1b696f4e50a
Miroshkin <vovamiroshkin2011@gmail.com>
2025-12-13 20:59:45 +0300

    Обнова

--- api/score.js
+++ api/score.js
@@ -2,11 +2,11 @@ export default async function score(req, res) {
     let score
     try {
         score = await req.text()
-        console.log(score)
+        console.log("Успех! score = " + score)
     }
     catch {
         score = "0"
-        console.log(score)
+        console.log("Неудача! score =" + score)
     }
     const secondsPerWeek = 60 * 60 * 24 * 7
     res.setHeader("Set-Cookie", `score=${score}; Max-Age=${secondsPerWeek}`)
