79d511bfc78e88408f607fefecb4ea7c3db0a931
Miroshkin <vovamiroshkin2011@gmail.com>
2025-12-13 21:33:25 +0300

    Долгожданое обновление

--- api/score.js
+++ api/score.js
@@ -1,16 +1,16 @@
+export const config = {
+    api: {
+        bodyParser: false
+    }
+}
 export default async function score(req, res) {
+    let scoreString = ""
     let score
-    const text = await req.text()
-    console.log("text = " + text)
-    try {
-        score = await req.text()
-        console.log("Успех! score = " + score)
-    }
-    catch {
-        score = "0"
-        console.log("Неудача! score = " + score)
-    }
     const secondsPerWeek = 60 * 60 * 24 * 7
-    res.setHeader("Set-Cookie", `score=${score}; Max-Age=${secondsPerWeek}`)
-    res.end()
+    req.on("data", chunk => scoreString += chunk.toString())
+    req.on("end", () => {
+        score = scoreString || "0"
+        res.setHeader("Set-Cookie", `score=${score}; Max-Age=${secondsPerWeek}`)
+        res.end()
+    })  
 }
