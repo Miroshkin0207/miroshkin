6828dc9dd091c9334fe83a10644c86afa7e980b2
Miroshkin <vovamiroshkin2011@gmail.com>
2025-12-13 11:10:51 +0300

    Обновление кликера 3

--- api/score.js
+++ api/score.js
@@ -1,5 +1,10 @@
 export default async function score(req, res) {
-    const score = await req.text()
+    try {
+        const score = await req.text()
+    }
+    catch {
+        const score = "0"
+    }
     const secondsPerWeek = 60 * 60 * 24 * 7
     res.setHeader("Set-Cookie", `score=${score}; Max-Age=${secondsPerWeek}`)
     res.end()
