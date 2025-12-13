c4a304495b5027a8dda494afba61ff9bc198e0d5
Miroshkin <vovamiroshkin2011@gmail.com>
2025-12-13 20:04:56 +0300

    Проверочка

--- api/get-score.js
+++ api/get-score.js
@@ -1,17 +1,20 @@
 export default function getScore(req, res) {
     const cookieString = req.headers.cookie || ""
+    console.log("cookieString = " + cookieString)
     if (cookieString == "") {
         return res.send("0")
     }
     const scoreIndex = cookieString.indexOf("score=")
+    console.log("scoreIndex = " + scoreIndex)
     let scoreString = scoreIndex != -1 ? cookieString.substring(scoreIndex + 6) : 0
     let score = ""
+    console.log("scoreString = " + scoreString)
     for (let i = 0; i < scoreString.length; i++) {
         if (scoreString[i] == ";") {
             break
         }
         score += scoreString[i]
     }
-
+    console.log("score = " + score)
     return res.send(score)
 }
