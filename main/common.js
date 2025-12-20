39163fcebb0c0d41369488422a5a8c28d761013d
Miroshkin <vovamiroshkin2011@gmail.com>
2025-12-20 22:07:30 +0300

    Обновление

--- main/common.js
+++ main/common.js
@@ -1,6 +1,7 @@
 const script = document.createElement("script")
 script.src = "https://cdn.jsdelivr.net/gh/scottschiller/Snowstorm/snowstorm.js"
 script.onload = () => {
-    snowStorm.snowCharacter = "•"
+    snowStorm.snowCharacter = "•",
+    snowStorm.excludeMobile = false
 }
 document.body.appendChild(script)
