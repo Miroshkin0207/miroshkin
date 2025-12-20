0b1bbcfdcb77e99077415124f95afe4e8470306a
Miroshkin <vovamiroshkin2011@gmail.com>
2025-12-20 22:16:11 +0300

    Оптимизация

--- main/common.js
+++ main/common.js
@@ -1,7 +1,11 @@
 const script = document.createElement("script")
 script.src = "https://cdn.jsdelivr.net/gh/scottschiller/Snowstorm/snowstorm.js"
 script.onload = () => {
-    snowStorm.snowCharacter = "•",
+    snowStorm.snowCharacter = "•"
     snowStorm.excludeMobile = false
+    if (matchMedia("(max-width: 999px)").matches)
+    {
+        snowStorm.flakesMax = 30
+    }
 }
 document.body.appendChild(script)
