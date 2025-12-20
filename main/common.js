2ff201e5e7ff6663737f55ec7f595eb93f1e578f
Miroshkin <vovamiroshkin2011@gmail.com>
2025-12-20 21:57:06 +0300

    Снежинки добавлены

---
+++ main/common.js
@@ -0,0 +1,6 @@
+const script = document.createElement("script")
+script.src = "https://cdn.jsdelivr.net/gh/scottschiller/Snowstorm/snowstorm.js"
+script.onload = () => {
+    snowStorm.snowCharacter = "•"
+}
+document.body.appendChild(script)
