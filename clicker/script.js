1a36522dfe64114ccdfecb39ea884399220cad3a
Miroshkin0207 <vovamiroshkin2011@gmail.com>
2026-02-06 13:02:02 +0300

    Update script.js

--- clicker/script.js
+++ clicker/script.js
@@ -69,7 +69,7 @@ function mouseEnd() {
 
 const table = [
     {name: "Сигма Влдмр", score: 666},
-    {name: "Левин", score: 200},
+    {name: "Министерство Казинака", score: 777},
     {name: "Козёл в очках с бородкой", score: 5000},
     {name: "Пряничный раб Козла в очках", score: 1760},
     {name: "TON-618", score: 2500},
